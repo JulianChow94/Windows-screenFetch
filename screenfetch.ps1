78353d31c556cb0dc44f75ab6545c14909e39e14
JulianChow94 <julianchow@outlook.com>
2016-07-02 21:42:44 -0400

    Replaced package with motherboard, since package search takes too much computation time

--- screenfetch.ps1
+++ screenfetch.ps1
@@ -1,93 +1,112 @@
-# Screenfetch for powershell
-# Author Julian Chow
+#### Screenfetch for powershell
+#### Author Julian Chow
 
+####### Information Collection #########
 
-# Some important Variables for convenience
+## Resolution Information
 $Horizontal = (gwmi -Class Win32_VideoController).CurrentHorizontalResolution | select -f 2;
 $Vertical = (gwmi -Class Win32_VideoController).CurrentVerticalResolution | select -f 1;
+
+## Uptime Information
 $uptime = ((gwmi Win32_OperatingSystem).ConvertToDateTime((gwmi Win32_OperatingSystem).LocalDateTime) - 
            (gwmi Win32_OperatingSystem).ConvertToDateTime((gwmi Win32_OperatingSystem).LastBootUpTime));
+
+## Disk Information
+# Available Space
 $FreeDiskSize = (gwmi Win32_LogicalDisk).FreeSpace | select -f 1;
 $FreeDiskSizeGB = $FreeDiskSize / 1073741824;
 $FreeDiskSizeGB = "{0:N0}" -f $FreeDiskSizeGB;
+# Total Space
 $DiskSize = (gwmi Win32_LogicalDisk).size | select -f 1;
 $DiskSizeGB = $DiskSize / 1073741824;
 $DiskSizeGB = "{0:N0}" -f $DiskSizeGB;
 $DiskPercent = ($FreeDiskSizeGB / $DiskSizeGB) * 100;
 $DiskPercent = "{0:N0}" -f $DiskPercent;
 
+## Environment Information
+$Username = $env:username;
+$Machine = (gwmi Win32_OperatingSystem).CSName;
+$OS = (gwmi Win32_OperatingSystem).Caption;
+$BitVer = (gwmi Win32_OperatingSystem).OSArchitecture;
+$Kernel = (gwmi Win32_OperatingSystem).Version;
+
+## Hardware Information
+$Motherboard = Get-CimInstance Win32_BaseBoard | Select-Object Manufacturer, Product;
+$CPU = (((gwmi Win32_Processor).Name) -replace '\s+', ' ');
+$GPU = (gwmi Win32_DisplayConfiguration).DeviceName;
+$FreeRam = ([math]::Truncate((gwmi Win32_OperatingSystem).FreePhysicalMemory / 1KB)); 
+$TotalRam = ([math]::Truncate((gwmi Win32_ComputerSystem).TotalPhysicalMemory / 1MB));
+$RamPercent = ($FreeRam / $TotalRam) * 100;
+$RamPercent = "{0:N0}" -f $RamPercent;
+
+
+####### Printing Output #########
+
 # Line 1 - HostName
 Write-Host "                         ....::::       " -f Cyan -NoNewline;
-Write-Host $env:username -f red -nonewline; 
+Write-Host $Username -f red -nonewline; 
 Write-Host "@" -f gray -nonewline; 
-Write-Host (gwmi Win32_OperatingSystem).CSName -f red;
+Write-Host $Machine -f red;
 
-#Line 2 - OS
+# Line 2 - OS
 Write-Host "                 ....::::::::::::       " -f Cyan -NoNewline;
 Write-Host "OS: " -f Red -NoNewline;
-Write-Host (gwmi Win32_OperatingSystem).Caption (gwmi Win32_OperatingSystem).OSArchitecture;
+Write-Host $OS $BitVer;
 
-#Line 3 - Kernel
+# Line 3 - Kernel
 Write-Host "        ....:::: ::::::::::::::::       " -f Cyan -NoNewline;
 Write-Host "Kernel: " -f Red -nonewline;
-Write-Host (gwmi Win32_OperatingSystem).Version;
+Write-Host $Kernel;
 
-#Line 4 - Uptime
+# Line 4 - Uptime
 Write-Host "....:::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
 Write-Host "Uptime: " -f Red  -nonewline;
 Write-Host $uptime.Days"d " $uptime.Hours"h " $uptime.Minutes"m " $uptime.Seconds"s " -separator "";
 
-#Line 5 - Packages
+# Line 5 - Motherboard
 Write-Host ":::::::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
-Write-Host "Packages: " -f Red -nonewline; 
-Write-Host (gwmi Win32_Product).Count;
+Write-Host "Motherboard: " -f Red -nonewline; 
+Write-Host $Motherboard.Product;
 
-#Line 6 - Shell
+# Line 6 - Shell (Hardcoded since it is unlikely anybody can run this without powershell)
 Write-Host ":::::::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
 Write-Host "Shell: " -f Red -nonewline; 
 Write-Host "Powershell 3.0"
 
-#Line 7 - Resolution
+# Line 7 - Resolution
 Write-Host ":::::::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
 Write-Host "Resolution: " -f Red -NoNewline; 
 Write-Host $Horizontal "x" $Vertical;
 
-#Line 8 - Windows Manager (HARDCODED)
+# Line 8 - Windows Manager (HARDCODED, sorry bbzero users)
 Write-Host ":::::::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
 Write-Host "Window Manager: " -f Red -nonewline; 
 Write-Host "DWM";
 
-#Line 10 - Font (HARDCODED)
+# Line 10 - Font (HARDCODED)
 Write-Host "................ ................       " -f Cyan -NoNewline;
 Write-Host "Font: " -f Red -nonewline; 
 Write-Host "Segoe UI";
 
-#Line 11 - CPU
+# Line 11 - CPU
 Write-Host ":::::::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
 Write-Host "CPU: " -f Red -nonewline; 
-Write-Host (((gwmi Win32_Processor).Name) -replace '\s+', ' ');
+Write-Host $CPU;
 
-#Line 12 - GPU
+# Line 12 - GPU
 Write-Host ":::::::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
 Write-Host "GPU: " -f Red -nonewline; 
-Write-Host (gwmi Win32_DisplayConfiguration).DeviceName;
+Write-Host $GPU;
 
-#Line 13 - Ram
+# Line 13 - Ram
 Write-Host ":::::::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
 Write-Host "RAM: " -f Red -nonewline;
-$FreeRam = ([math]::Truncate((gwmi Win32_OperatingSystem).FreePhysicalMemory / 1KB)); 
-Write-Host $FreeRam -NoNewline;
-Write-Host "MB / " -NoNewline;
-$TotalRam = ([math]::Truncate((gwmi Win32_ComputerSystem).TotalPhysicalMemory / 1MB));
-Write-Host $TotalRam -NoNewline;
-$RamPercent = ($FreeRam / $TotalRam) * 100;
-$RamPercent = "{0:N0}" -f $RamPercent;
-Write-Host "MB" -NoNewline;
+Write-Host $FreeRam "MB / $TotalRam MB" -NoNewline;
 Write-Host " (" -NoNewline
 Write-Host $RamPercent"%" -f Green -NoNewline;
 Write-Host ")";
 
-#Line 13 - Disk Usage
+# Line 13 - Disk Usage
 Write-Host "'''':::::::::::: ::::::::::::::::       " -f Cyan -NoNewline;
 Write-Host "Disk: " -f Red -NoNewline;
 Write-Host $FreeDiskSizeGB"GB" " / " $DiskSizeGB"GB" -NoNewline;
@@ -95,8 +114,9 @@ Write-Host " (" -NoNewline;
 Write-Host $DiskPercent"%" -f Green -NoNewline;
 Write-Host ")"; 
 
-Write-Host "        '''':::: ::::::::::::::::       " -f Cyan #-NoNewline;
-Write-Host "                 ''''::::::::::::       " -f Cyan #-NoNewline;
-Write-Host "                         ''''::::       " -f Cyan #-NoNewline;
+# Empty Lines
+Write-Host "        '''':::: ::::::::::::::::       " -f Cyan;
+Write-Host "                 ''''::::::::::::       " -f Cyan;
+Write-Host "                         ''''::::       " -f Cyan;
 
 
